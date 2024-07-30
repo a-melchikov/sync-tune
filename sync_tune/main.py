d3272f44dd5ecec2dc1867025228f28ce6826a90
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-07-30 17:59:47 +0400

    Код переписан на музыкальный плеер, который работает на WebSocket.
    - Добавлен метод send_welcome_message, который отправляет всем пользователям (кроме самого пользователя) сообщение о том, что присоединился новый пользователь

--- sync_tune/main.py
+++ sync_tune/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from typing import List
+from typing import List, Dict, Union
 
 app = FastAPI()
 
@@ -13,24 +13,29 @@ class ConnectionManager:
 	Управление подключениями и обменом сообщениями между клиентами.
 
 	Атрибуты:
-	connected_clients (list[WebSocket]): список подключенных клиентов (объектов WebSocket).
-	message_queue (list[str]): очередь сообщений для отправки клиентам.
+	connected_clients (List[Dict[str, Union[WebSocket, str]]]): список подключенных клиентов (объектов WebSocket).
+	message_queue (List[str]): очередь сообщений для отправки клиентам.
 	"""
 
 	def __init__(self):
-		self.connected_clients: list[WebSocket, str] = []
-		self.message_queue: list[str] = []
+		self.connected_clients: List[Dict[str, Union[WebSocket, str]]] = []
+		self.message_queue: List[str] = []
 
 	async def connect(self, websocket: WebSocket, username: str):
 		await websocket.accept()
 		self.connected_clients.append({'websocket': websocket, 'username': username})
 
-	def disconnect(self, websocket: WebSocket, username: str):
-		self.connected_clients.remove({"websocket": websocket, "username": username})
+	def disconnect(self, websocket: WebSocket):
+		self.connected_clients = [client for client in self.connected_clients if client['websocket'] != websocket]
 
 	async def send_personal_message(self, websocket: WebSocket, username: str):
-		welcome_message = f"Привет, {username}! Добро пожаловать в чат!"
+		welcome_message = f"Привет, {username}! Добро пожаловать в музыкальный плеер!"
 		await websocket.send_text(welcome_message)
+
+	async def send_welcome_message(self, websocket: WebSocket, username: str):
+		for client in self.connected_clients:
+			if client['websocket'] != websocket:
+				await client['websocket'].send_text(f"Пользователь, {username} присоединился в плеер!")
 
 	async def send_messages_from_queue(self, websocket: WebSocket):
 		for message in self.message_queue:
@@ -52,18 +57,18 @@ async def websocket_endpoint(websocket: WebSocket, username: str):
 	await manager.connect(websocket, username)
 	await manager.send_personal_message(websocket, username)
 	await manager.send_messages_from_queue(websocket)
+	await manager.send_welcome_message(websocket, username)
 
 	try:
 		while True:
 			data = await websocket.receive_text()
-			message = f"{username}: {data}"
-			manager.append_new_message(message)
-			await manager.broadcast(message)
+			manager.append_new_message(data)
+			await manager.broadcast(data)
 	except WebSocketDisconnect:
 		manager.disconnect(websocket, username)
-		await manager.broadcast(f"Пользователь {username} покинул чат!")
+		await manager.broadcast(f"Пользователь {username} покинул плеер!")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def chat_interface(request: Request):
-	return templates.TemplateResponse("chat.html", {"request": request})
+	return templates.TemplateResponse("index.html", {"request": request})
